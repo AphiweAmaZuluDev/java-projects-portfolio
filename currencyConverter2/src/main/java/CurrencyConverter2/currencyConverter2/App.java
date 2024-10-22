e53824dca90ffa5f9b1edabaaed761ab299c4a51
AphiweAmaZuluDev <azulu058@gmail.com>
2024-10-22 18:42:31 +0200

    Currency Converter

--- currencyConverter2/src/main/java/CurrencyConverter2/currencyConverter2/App.java
+++ currencyConverter2/src/main/java/CurrencyConverter2/currencyConverter2/App.java
@@ -5,6 +5,11 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Time;
 import java.util.HashMap;
 import java.util.Scanner;
 
@@ -12,11 +17,25 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
+import com.mysql.cj.xdevapi.Statement;
+
 
 public class App 
 {
+	//Global Variables
+	
+	static Connection con;
+	static int transactions = 1;
+	
+	
+	
     public static void main( String[] args ) throws IOException, ParseException
     {
+    	// Connect to database
+    	App app = new App();
+    	app.createConnection();
+    	
+    	
     	HashMap<Integer, String> currency_codes = new HashMap<Integer, String>();
     	
     	// Add currency codes
@@ -25,6 +44,7 @@ public class App
     	currency_codes.put(3, "GBP");
     	currency_codes.put(4, "CHF");
     	currency_codes.put(5, "JPY");
+    	
     	
     	int from, to;
     	String from_code, to_code;
@@ -57,6 +77,8 @@ public class App
     	 
     	 System.out.println("You have chosen: " + from_code);
     	 
+    	 
+    	 
     	 System.out.println("Which currency do you wish to convert to?");
     	 
     	 System.out.println("1: USD (US Dollar) \t 2: EUR (Euro)"
@@ -85,14 +107,29 @@ public class App
     	 amount = (double)scn.nextFloat();
     	 
     	 
-    	 double result = (double)sendHttpGETRequest(from_code, to_code, amount);
+    	 double result = sendHttpGETRequest(from_code, to_code, amount);
     	 
     	 System.out.println("That will be " + result + " " + to_code);
     	 
     	 System.out.println("Thank you for using the currency converter.");
     	 
+    	 // Log transaction to database
+    	 
+    	 try {
+ 			java.sql.Statement statement = con.createStatement();
+ 			String dbOperation = "INSERT INTO LOGS VALUES(" + transactions + ", '" + from_code + "'," + amount + ",'" + to_code 
+ 					+ "'," + result + ", CURDATE(), CURTIME());";
+ 			statement.execute(dbOperation);
+ 			statement.close();
+ 		} catch (SQLException e) {
+ 			e.printStackTrace();
+ 		}
+    	 
+    	 transactions++;
+    	 
     }
     
+    // Send HTTP request to Currency converter API.
     private static double sendHttpGETRequest(String from_code, String to_code, Double amount) throws IOException, ParseException {
     	String GET_URL = "https://v6.exchangerate-api.com/v6/e56d6d73549d4a1b83e18f66/pair/" + from_code + "/"
     			+ to_code + "/" + amount.toString();
@@ -119,8 +156,38 @@ public class App
     		System.out.println("GET request failed!");
     		System.out.println(response_code);
     	}
-    	double result = (double)obj.get("conversion_result");
+    	
+    	double result = 0;
+    	
+    	Object valueFromAPI = obj.get("conversion_result");
+    	
+    	if (valueFromAPI instanceof Long) {
+    	    Long longValue = (Long) valueFromAPI;
+    	    Double doubleValue = longValue.doubleValue();
+    	    System.out.println("Converted value: " + doubleValue);
+    	    result = doubleValue;
+    	} else if (valueFromAPI instanceof Double) {
+    	    Double doubleValue = (Double) valueFromAPI;
+    	    System.out.println("Value is already a Double: " + doubleValue);
+    	    result = doubleValue;
+    	} else {
+    	    System.out.println("Unexpected value type: " + valueFromAPI.getClass().getName());
+    	}
+    	
     	return result;
-	}
+    }
+    
+    // Establish connection with Database
+    void createConnection() {
+    	try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/curr_converter_db", "root", "root");
+			System.out.println("Database Connection Successful!!");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+    }
 }
 
